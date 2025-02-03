243525b4336b2c40bb23749cf51ad8784f3c9716
om <omjha003@gmail.com>
2025-02-03 12:37:44 +0530

    Initial setup of the project

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+import express, { urlencoded } from "express"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv"
+import connectDB from "./utils/db.js"
+
+const app = express();
+dotenv.config()
+let port = process.env.PORT;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(urlencoded({extended : true}))
+const corsOptions = {
+    origin : "http://localhost:5173",
+    credentials : true
+}
+
+app.use(cors(corsOptions))
+
+app.get("/" , (req , res) => {
+    res.send("I am in home page")
+})
+
+app.listen(port , () => {
+    connectDB()
+    console.log(`Server is listening on port ${port}`);
+})
